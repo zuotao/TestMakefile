4fcd9eaeb083ea6d60f1c695c533b4fcca9d9f93
taoge <taoge@taogedeMac.lan>
2017-12-03 01:48:16 -0800

    重构

--- common.mk
+++ common.mk
@@ -1,14 +1,16 @@
 .PHONY:all clean
-
+# scan current directory sources files
 SRCS = $(wildcard *.c)
 OBJS = $(SRCS:.c=.o)
 DEPS = $(SRCS:.c=.d)
 BIN := $(addprefix $(BUILD_ROOT)/,$(BIN))
 
+# link object files directory
 LINK_OBJ_DIR = $(BUILD_ROOT)/app/link_obj
 $(shell mkdir -p $(LINK_OBJ_DIR))
 DEP_DIR = $(BUILD_ROOT)/app/dep
 $(shell mkdir -p $(DEP_DIR))
+# lib object files directory
 LIB_OBJ_DIR = $(BUILD_ROOT)/app/lib_obj
 $(shell mkdir -p $(LIB_OBJ_DIR))
 LIB_DIR = $(BUILD_ROOT)/lib
@@ -19,6 +21,7 @@ OBJ_DIR = $(LIB_OBJ_DIR)
 endif
 ifneq ("$(DLL)","")
 OBJ_DIR = $(LIB_OBJ_DIR)
+# for dll object files param
 PIC     = -fPIC
 endif
 
--- makefile
+++ makefile
@@ -1,22 +1,20 @@
-.PHONY:all clean
-export BUILD_ROOT = $(shell pwd)
-export HEAD_PATH = $(BUILD_ROOT)/inc
-all:
-	make -C lcd
-	make -C usb
-	make -C media
-	make -C math
-	make -C app
+.PHONY:all clean install uninstall
+include config.mk
 
+all:
+	@for dir in $(BUILD_DIR); \
+	do \
+		make -C $$dir; \
+	done
 clean:
 	rm -rf app/link_obj app/dep app/lib_obj mp3
 	rm -rf lib/*.a
 	rm -rf lib/*.so
-isntall:
+install:
 	cp mp3 /usr/bin
 	cp lib/libmath.so /usr/lib
 	cp lib/ext_lib/librmvb.so /usr/lib
 uninstall:
-	rm /usr/bin/mp3
+	rm -f /usr/bin/mp3
 	rm /usr/lib/libmath.so
 	rm /usr/lib/librmvb.so
